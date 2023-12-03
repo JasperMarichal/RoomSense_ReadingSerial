24fec2aa4e612a0bb954956954a6527500e9cfd9
Roman Gordon <roman.gordon@student.kdg.be>
2023-12-03 17:47:25 +0100

    Added command line arguments for ip, port and roomId

--- src/main/java/be/kdg/integration3/Main.java
+++ src/main/java/be/kdg/integration3/Main.java
@@ -12,21 +12,46 @@ public class Main {
     public static final int SAVE_BATCH_SIZE = 50;
 
     public static void main(String[] args) {
-        RawDataWriter writer;
+        String ip = "";
+        int port = 0;
+        int roomId = 0;
+
         try {
-            writer = new DBWriter(7, SAVE_BATCH_SIZE);
-        }catch (RuntimeException e) {
-            System.err.println(e.getMessage());
-            System.err.println("Falling back to JsonWriter...");
-            writer = new JsonWriter(SAVE_BATCH_SIZE);
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Argument " + i + ": " + args[i]);
+                String arg = args[i];
+                if (arg.startsWith("ip=")) {
+                    ip = arg.substring(arg.lastIndexOf("=") + 1);
+                } else if (arg.startsWith("port=")) {
+                    port = Integer.parseInt(arg.substring(arg.lastIndexOf("=") + 1));
+                } else if (arg.startsWith("room=")) {
+                    roomId = Integer.parseInt(arg.substring(arg.lastIndexOf("=") + 1));
+                }
+            }
+        } catch (NumberFormatException e){
+            System.out.println("Port or Room was not entered as an int");
         }
 
-        DataPreprocessor preprocessor = new SoundPreprocessor(writer);
-        DataReader reader = new TelnetRead(preprocessor, writer);
+        if (port == 0 || roomId == 0 || ip.isEmpty()){
+            System.err.println("You have not entered at least one of the runtime variables");
+            System.out.println("Ensure you have defined 'ip=ipAddress port=portNumber room=roomId'");
+        } else {
+            RawDataWriter writer;
+            try {
+                writer = new DBWriter(roomId, SAVE_BATCH_SIZE);
+            } catch (RuntimeException e) {
+                System.err.println(e.getMessage());
+                System.err.println("Falling back to JsonWriter...");
+                writer = new JsonWriter(SAVE_BATCH_SIZE);
+            }
 
-        while (true) {
-            reader.readData();
-            writer.saveAllData();
+            DataPreprocessor preprocessor = new SoundPreprocessor(writer);
+            DataReader reader = new TelnetRead(preprocessor, writer, ip, port);
+
+            while (true) {
+                reader.readData();
+                writer.saveAllData();
+            }
         }
     }
 }
